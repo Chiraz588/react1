af5bc97a21e2bd5e53e26fb2bdaf142de5999a35
Chiraz588 <75127710+Chiraz588@users.noreply.github.com>
2021-11-06 13:33:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Formulaire from './Form';
+import Nav from './navv';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Formulaire/>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
